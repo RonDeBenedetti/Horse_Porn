80d65f8fc5c3d8574aac20f720da5aaac7d9ff91
Ron De Benedetti <rdlandfill@gmail.com>
2011-07-08 03:52:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HorsePorn::Application.config.secret_token = 'aaedca3d0d8bbd48f87855fedcde644326b6f02c982b2ee64507a51c0504022e413d6e8273d2ed7715af3bbc18596f6c65cc57eb6e4d7b7cc6ed31cd883576ba'
